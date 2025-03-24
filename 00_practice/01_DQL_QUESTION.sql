a8fd438614f8a78caa8772a32c239bf83953f906
seokgeun_cho <choseokgeun@gmail.com>
2025-03-24 09:05:28 +0900

    수정 본 커밋

--- 00_practice/01_DQL_QUESTION.sql
+++ 00_practice/01_DQL_QUESTION.sql
@@ -6,8 +6,13 @@ use empdb;
 
     차태연
     전지연
-
-*/
+           */
+SELECT
+      emp_name
+  FROM
+      employee
+ WHERE
+      emp_name like '%연';
 
 -- 2. employee 테이블에서 전화번호 처음 3자리가 010이 아닌 사원의 이름, 전화번호를 출력하시오
 
@@ -16,17 +21,33 @@ use empdb;
     심봉선 0113654485
     윤은해 0179964233
     하동운 01158456632
-
-*/
+                      */
+select
+      emp_name
+    , phone
+ from
+      employee
+ where
+      phone not like '010%';
 
 -- 3. employee 테이블에서 메일주소 '_'의 앞이 4자이면서, DEPT_CODE가 D9 또는 D5이고 고용일이 90/01/01 ~ 01/12/31이면서, 월급이 270만원 이상인 사원의 전체 정보를 출력하시오
 
 /*
     출력
     201 송종기 631126-1548654 song_jk@ohgiraffers.com 01045686656 D9 J2 S1 6000000  200 2001-09-01 00:00:00 N
-
-
-*/
+                                                                                                             */
+select
+      *
+ from
+      employee
+ where
+      (email like '____\_%')
+   and
+      (dept_code='D9' or dept_code='D5')
+   and
+      (hire_date between '90/01/01' and '01/12/31')
+   and
+      salary >= 2700000;
 
 -- 4. employee테이블에서 현재 근무중인 사원을 이름 오름차순으로 정렬해서 출력.
 
@@ -35,10 +56,15 @@ use empdb;
     223,고두밋
     208,김해술
     ...
-
-*/
-
-
+                 */
+select
+      *
+ from
+      employee
+ where
+      quit_yn = 'N'
+ order by
+      emp_name;
 
 -- 5. 사원별 입사일, 퇴사일, 근무기간(일)을 조회하세요. 퇴사자 역시 조회되어야 합니다.
 
@@ -48,8 +74,15 @@ use empdb;
     노옹철 2001-01-01 00:00:00  8506  N
     송은희 1996-05-03 00:00:00  10210 N
     ...
-
-*/
+                                        */
+select
+      emp_name
+    , hire_date AS 입사일
+    , quit_date AS 퇴사일
+    , datediff(ifnull(quit_date, now()), hire_date) AS '근무기간(일)',
+      quit_yn
+ from
+      employee;
 
 
 -- 6. 재직 중이고 휴대폰 마지막 자리가 2인 직원 중 입사일이 가장 최근인 직원 3명의 사원번호,
@@ -63,14 +96,25 @@ use empdb;
         206	박나라	01096935222	2008-04-02 00:00:00	N
     */
 
+SELECT EMP_ID, EMP_NAME, PHONE, HIRE_DATE, QUIT_YN
+  FROM EMPLOYEE
+ WHERE PHONE LIKE '%2'
+  AND QUIT_YN <> 'Y'
+ ORDER BY HIRE_DATE DESC
+LIMIT 3;
+
 
 
 -- 7. <1단계> 전체 직원 중 연결된 관리자가 있는 직원의 인원을 출력하세요.
 -- 참고. 연결된 관리자가 있다는 것은 관리자사번이 NULL이 아님을 의미함
-
 
     /*
         출력
 
         16
     */
+
+SELECT COUNT(*)
+  FROM EMPLOYEE
+ WHERE MANAGER_ID IS NOT NULL
+ORDER BY EMP_NAME
